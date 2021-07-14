5b493a6ad878e15acd924eb19b20b8f7b4e19e2b
ituniti <inituniti@gmail.com>
2021-07-14 19:48:07 +0300

    refactor: React sprint 11

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,50 @@
 import React from "react";
+import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Card(props) {
+function Card({ card, onCardClick, onCardLike, onCardDelete }) {
+  const currentUser = React.useContext(CurrentUserContext);
+
+  const isOwn = card.owner._id === currentUser._id;
+  const cardDeleteButtonClassName = `element__remove-button ${
+    isOwn ? " " : "element__remove-button_invisible"
+  }`;
+
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const cardLikeButtonClassName = `element__vector ${
+    isLiked ? "element__vector_active" : ""
+  }`;
+
   function handleClick() {
-    props.onCardClick(props.card);
+    onCardClick(card);
+  }
+
+  function handleLikeClick() {
+    onCardLike(card);
+  }
+
+  function handleDeleteClick() {
+    onCardDelete(card);
   }
 
   return (
     <div className="element">
-      <button className="element__remove-button element__remove-button_invisible"></button>
+      <button
+        className={cardDeleteButtonClassName}
+        onClick={handleDeleteClick}></button>
       <img
         className="element__image"
-        src={props.card.link}
-        alt={props.card.name}
+        src={`${card.link}`}
+        alt={`${card.name}`}
         onClick={handleClick}
       />
       <div className="element__wrapper">
-        <h2 className="element__text"> {props.name} </h2>
+        <h2 className="element__text">{card.name}</h2>
         <div className="element__block-with-likes">
-          <button className="element__vector" type="button"></button>
-          <p className="element__number-of-likes">{props.likes}</p>
+          <button
+            className={cardLikeButtonClassName}
+            type="button"
+            onClick={handleLikeClick}></button>
+          <p className="element__number-of-likes">{card.likes.length}</p>
         </div>
       </div>
     </div>
